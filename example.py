65f5eea26aae7ab50f3413f2678807faa0c9b4e2
Xenymor <101249409+Xenymor@users.noreply.github.com>
2025-10-23 17:54:13 +0200

    Finish 0-4

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 from SlimeGameLibrary import *
 
 
-InitializeSlime("0-3", "Black", "Germany", 2, 5, 3)
+InitializeSlime("0-4", "Black", "Germany", 2, 5, 3)
 
 positionSign = RelativePosition(Self.TeamSpawn, "Backward")
+
+DebugDrawDisc(positionSign, .1, .1, "Black")
 
 ballVel = Ball.Velocity
 yVel = ballVel.y
@@ -14,11 +16,19 @@ flighttime = (yVel + Sqrt(yVel * yVel + (2 * gravity * ballPos.y))) / gravity
 
 landPos = Vector3(ballVel.x * flighttime + ballPos.x, 0, ballVel.z * flighttime + ballPos.z)
 
-moveTo = landPos
+DebugDrawDisc(landPos, .1, .1, "Green")
+
+moveToBall = Ball.IsSelfSide #(landPos.z * positionSign.z) >= 0
+
+targetPos = landPos + positionSign * 0.1 #ConditionalSetVector3(moveToBall, landPos + positionSign * 0.1, RelativePosition(Self.TeamSpawn, "Backward"))
+
+moveTo = targetPos
+
+DebugDrawDisc(moveTo, .1, .1, "Blue")
 
 distanceToBall = Distance(Ball.Position, Self.Position)
 jumpCondition = distanceToBall <= 2
 
 SlimeController(moveTo, jumpCondition)
 
-SaveData("C:/Users/timon/Documents/Spiele/Slime Volleyball/AIComp_Data/Saves/0-3.txt", "auto")
+SaveData("C:/Users/timon/Documents/Spiele/Slime Volleyball/AIComp_Data/Saves/0-4.txt", "auto")
