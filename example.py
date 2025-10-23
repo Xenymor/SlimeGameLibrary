5fd5591e8a6eee4e384fbc97fd0a4b8aae167b54
Xenymor <101249409+Xenymor@users.noreply.github.com>
2025-10-23 16:50:22 +0200

    Refactor

--- example.py
+++ example.py
@@ -8,7 +8,9 @@ positionSign = RelativePosition(Self.TeamSpawn, "Backward")
 ballVel = Ball.Velocity
 yVel = ballVel.y
 ballPos = Ball.Position
-flighttime = ((yVel)+Sqrt(yVel*yVel+(34*ballPos.y)))/17
+gravity = 17
+
+flighttime = (yVel + Sqrt(yVel * yVel + (2 * gravity * ballPos.y))) / gravity
 
 landPos = Vector3(ballVel.x * flighttime + ballPos.x, 0, ballVel.z * flighttime + ballPos.z)
 
@@ -19,4 +21,4 @@ jumpCondition = distanceToBall <= 2
 
 SlimeController(moveTo, jumpCondition)
 
-SaveData("C:/Users/timon/Documents/Spiele/Slime Volleyball/AIComp_Data/Saves/0-3.txt", "grid")
+SaveData("C:/Users/timon/Documents/Spiele/Slime Volleyball/AIComp_Data/Saves/0-3.txt", "auto")
